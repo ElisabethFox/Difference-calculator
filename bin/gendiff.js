fcde25cdf4ba37b2dd906b9535632e0dcd3c905d
ElisabethFox <lisa.black.fox17@gmail.com>
2022-12-13 21:18:19 +0300

    add format type and arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,7 @@ const program = new Command();
   program
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
+    .option('-f, --format <type>', 'output format')
+    .arguments('<filepath1> <filepath2>');
     
 program.parse();
