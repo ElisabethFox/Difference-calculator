d14103546a18ded53bafa4fd1d014c4bab1a920e
ElisabethFox <lisa.black.fox17@gmail.com>
2022-12-25 18:06:52 +0300

    the result is presented as a string

--- src/index.js
+++ src/index.js
@@ -13,21 +13,20 @@ const findDifferences = (obj1, obj2) => {
     const commonKey = `  ${key}`;
 
     if (!Object.hasOwn(obj2, key)) {
-      acc[minusKey] = obj1[key];
+      acc.push(` ${minusKey}: ${obj1[key]}`);
     } else if (!Object.hasOwn(obj1, key)) {
-      acc[plusKey] = obj2[key];
+      acc.push(` ${plusKey}: ${obj2[key]}`);
     } else if (obj1[key] !== obj2[key]) {
-      acc[minusKey] = obj1[key];
-      acc[plusKey] = obj2[key];
+      acc.push(` ${minusKey}: ${obj1[key]}\n ${plusKey}: ${obj2[key]}`);
     } else {
-      acc[commonKey] = obj1[key];
-    }
+      acc.push(` ${commonKey}: ${obj1[key]}`);
+    };
 
     return acc;
   };
 
-  const result = keys.reduce(cb, {});
-  return JSON.stringify(result, null, '\n');
+  const result = keys.reduce(cb, []).join('\n');
+  return `{\n${result}\n}`;
 };
 
 const getPath = (filename) => path.resolve(process.cwd(), filename);
