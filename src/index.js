45ac91ce72e661f8bf56aea195f571a581c7022b
ElisabethFox <lisa.black.fox17@gmail.com>
2023-01-18 19:13:22 +0300

    change in project structureэ

--- src/index.js
+++ src/index.js
@@ -1,35 +1,7 @@
 import { readFileSync } from 'fs';
-import _ from 'lodash';
 import path from 'path';
 import parser from './parsers.js';
-
-const findDifferences = (obj1, obj2) => {
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-  const keys = _.union(keys1, keys2).sort();
-
-  const cb = (acc, key) => {
-    const minusKey = `- ${key}`;
-    const plusKey = `+ ${key}`;
-    const commonKey = `  ${key}`;
-
-    if (!Object.hasOwn(obj2, key)) {
-      acc.push(`  ${minusKey}: ${obj1[key]}`);
-    } else if (!Object.hasOwn(obj1, key)) {
-      acc.push(`  ${plusKey}: ${obj2[key]}`);
-    } else if (obj1[key] !== obj2[key]) {
-      acc.push(`  ${minusKey}: ${obj1[key]}`);
-      acc.push(`  ${plusKey}: ${obj2[key]}`);
-    } else {
-      acc.push(`  ${commonKey}: ${obj1[key]}`);
-    }
-
-    return acc;
-  };
-
-  const result = keys.reduce(cb, []).join('\n');
-  return `{\n${result}\n}`;
-};
+import style from './stylish.js';
 
 const getPath = (filename) => path.resolve(process.cwd(), filename);
 const getFormat = (filepath) => path.extname(filepath).slice(1);
@@ -45,5 +17,5 @@ export default (file1, file2) => {
   const format2 = getFormat(path2);
   const obj2 = parser(data2, format2);
 
-  return findDifferences(obj1, obj2);
+  return style(obj1, obj2);
 };
