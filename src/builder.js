fdc858236a6a9a8e208b0422a3e2fed94981771f
ElisabethFox <lisa.black.fox17@gmail.com>
2023-01-28 18:04:16 +0300

    fix sort method

--- src/builder.js
+++ src/builder.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 const buildTree = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
-  const keys = _.union(keys1, keys2).sort();
+  const keys = _.sortBy(_.union(keys1, keys2));
 
   return keys.map((key) => {
     if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
