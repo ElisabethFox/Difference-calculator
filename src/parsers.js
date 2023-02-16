05d456e245cdb3e6d44a50abee3961bddc118cab
ElisabethFox <lisa.black.fox17@gmail.com>
2023-02-16 20:08:44 +0300

    delete switch/case and add object parsers

--- src/parsers.js
+++ src/parsers.js
@@ -1,14 +1,9 @@
 import yaml from 'js-yaml';
 
-export default (file, extension) => {
-  switch (extension) {
-    case 'json':
-      return JSON.parse(file);
-    case 'yml':
-      return yaml.load(file);
-    case 'yaml':
-      return yaml.load(file);
-    default:
-      throw new Error(`This extension is not supported: ${extension}`);
-  }
+const parsers = {
+  json: JSON.parse,
+  yml: yaml.load,
+  yaml: yaml.load,
 };
+
+export default (data, extension) => parsers[extension](data);
