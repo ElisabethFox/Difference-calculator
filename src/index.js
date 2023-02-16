ab75ea5ce7db358ab8f3db9775cf4e36c5e88868
ElisabethFox <lisa.black.fox17@gmail.com>
2023-02-16 20:09:33 +0300

    rename formatter to formatDiff

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs';
 import path from 'path';
 import parser from './parsers.js';
 import buildTree from './builder.js';
-import formatter from './formatters/index.js';
+import formatDiff from './formatters/index.js';
 
 const getPath = (filename) => path.resolve(process.cwd(), filename);
 const getFormat = (filepath) => path.extname(filepath).slice(1);
@@ -16,5 +16,5 @@ export default (file1, file2, formatName = 'stylish') => {
   const data2 = readFileSync(path2, 'utf8');
   const obj2 = parser(data2, getFormat(path2));
 
-  return formatter(buildTree(obj1, obj2), formatName);
+  return formatDiff(buildTree(obj1, obj2), formatName);
 };
